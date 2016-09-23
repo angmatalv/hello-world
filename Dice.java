4c1c062f19717d6685f7412022cecafd27d01c21
angmatalv <angmatalv@gmail.com>
2016-09-24 00:45:57 +0200

    Add files via upload

---
+++ Dice.java
@@ -0,0 +1,33 @@
+package test.tools;
+
+import java.util.Random;
+
+/**
+ * Dice class for Test001.
+ * 
+ * @author Angel Mate
+ *
+ */
+
+public class Dice {
+
+	/**
+	 * Arbitrary value to be added to the result of rolls.
+	 */
+
+	private static final int ARBITRARY_VALUE = 1;
+
+	/**
+	 * Rolls a dice. An arbitrary value is added to the result, to get values
+	 * between 1 and the number of sides, both inclusive.
+	 * 
+	 * @param size
+	 *            The number of sides the dice will have.
+	 * @return The result of rolling the dice.
+	 */
+
+	public static int roll(int size) {
+		Random randomizer = new Random();
+		return randomizer.nextInt(20) + ARBITRARY_VALUE;
+	}
+}
